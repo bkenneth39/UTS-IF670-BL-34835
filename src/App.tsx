48ee0285c1d09b28ea02e27c6c937c9a2b0ecf14
bkenneth39 <bryankenneth0309@gmail.com>
2021-10-13 09:49:45 +0700

    Unused component cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -18,7 +17,7 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-import {alert, list, mailOutline, people, person, settings, star, videocamOutline} from 'ionicons/icons'
+import {people, person, star} from 'ionicons/icons'
 /* Theme variables */
 import './theme/variables.css';
 import DaftarCalon from './pages/daftarCalon';
